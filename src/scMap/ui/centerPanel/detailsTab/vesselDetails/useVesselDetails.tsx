d208559c983ed952e47d82eb8f9b086b371fe41e
Dan Sapp <dan.sapp@storable.com>
2026-05-18 14:43:24 -0500

    lint

--- src/scMap/ui/centerPanel/detailsTab/vesselDetails/useVesselDetails.tsx
+++ src/scMap/ui/centerPanel/detailsTab/vesselDetails/useVesselDetails.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { type vesselState, type Vessel, setSelectedVessel } from "@/store/slices/vesselSlice";
 import { CameraContext } from "@/map/types";
