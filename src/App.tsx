cc0ea3e6b7f8ee983d9d36bde3ecd31c98a5ad40
Dan Sapp <dan.sapp@storable.com>
2026-05-21 16:40:03 -0500

    mobile test

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import SCMap from "./scMap/SCMap";
 
 const App = () => {
   return (
-    <div className="h-[calc(100%-2.5rem)] md:h-screen w-screen overflow-hidden flex">
+    <div className="pt-[env(safe-area-inset-top)] h-screen w-screen overflow-hidden flex">
       <SCMap />
     </div>
   );
