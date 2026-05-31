0e8eface17d138dbb935186642d029e61b630483
Dan Sapp <dan.sapp@storable.com>
2026-05-30 22:02:19 -0500

    Update DrawTab.tsx

--- src/scMap/ui/centerPanel/drawTab/DrawTab.tsx
+++ src/scMap/ui/centerPanel/drawTab/DrawTab.tsx
@@ -4,12 +4,12 @@ import { cn } from "@/lib/utils";
 import DrawDetails from "./drawDetails/DrawDetails";
 import { Separator } from "@/components/ui/separator";
 
-export const wrapperStyles = cn(["group flex flex-col items-center justify-center gap-px", "w-14 md:w-18 xl:w-22 "]);
+export const wrapperStyles = cn(["group flex flex-col items-center justify-center gap-px"]);
 export const buttonStyles = cn([
   "flex justify-center items-center rounded-xl",
   // "border border-emerald-400/10 group-hover:border-emerald-400/40",
   "bg-slate-700/60 border border-emerald-400/10 hover:bg-slate-700/90 group-hover:border-emerald-400/40",
-  "w-8 h-8 md:w-10 md:h-10 xl:w-12 xl:h-12 p-0",
+  "w-6 h-6 md:w-8 md:h-8 xl:w-10 xl:h-10 p-0",
 ]);
 export const buttonTextStyles = "text-(--text)/70 text-xs group-hover:text-emerald-400/70";
 export const iconStyles = "text-(--text)/70 lg:size-4 xl:size-6 group-hover:text-emerald-400/70";
@@ -17,38 +17,33 @@ const DrawTab = () => {
   const { handleChangeDrawMode, drawMode } = useDrawTab();
 
   return (
-    <div className="flex justify-center items-center h-full gap-3 p-3">
+    <div className="flex justify-center items-center h-full">
       <DrawDetails />
       <Separator orientation="vertical" />
-      <div className="flex flex-col gap-3 h-full">
-        <div className="w-full flex flex-col justify-center gap-1 md:gap-3">
-          <div className={wrapperStyles}>
-            <button
-              className={cn([buttonStyles, drawMode === "point" ? "bg-emerald-400/35" : ""])}
-              onClick={() => handleChangeDrawMode("point")}
-            >
-              <Dot className={iconStyles} />
-            </button>
-            <span className={buttonTextStyles}>Point</span>
-          </div>
-          <div className={wrapperStyles}>
-            <button
-              className={cn([buttonStyles, drawMode === "polyline" ? "bg-emerald-400/35" : ""])}
-              onClick={() => handleChangeDrawMode("polyline")}
-            >
-              <Slash className={iconStyles} />
-            </button>
-            <span className={buttonTextStyles}>Line</span>
-          </div>
-          <div className={wrapperStyles}>
-            <button
-              className={cn([buttonStyles, drawMode === "polygon" ? "bg-emerald-400/35" : ""])}
-              onClick={() => handleChangeDrawMode("polygon")}
-            >
-              <Hexagon className={iconStyles} />
-            </button>
-            <span className={buttonTextStyles}>Polygon</span>
-          </div>
+      <div className="flex flex-col gap-3 h-full p-3">
+        <div className={wrapperStyles}>
+          <button
+            className={cn([buttonStyles, drawMode === "point" ? "bg-emerald-400/35" : ""])}
+            onClick={() => handleChangeDrawMode("point")}
+          >
+            <Dot className={iconStyles} />
+          </button>
+        </div>
+        <div className={wrapperStyles}>
+          <button
+            className={cn([buttonStyles, drawMode === "polyline" ? "bg-emerald-400/35" : ""])}
+            onClick={() => handleChangeDrawMode("polyline")}
+          >
+            <Slash className={iconStyles} />
+          </button>
+        </div>
+        <div className={wrapperStyles}>
+          <button
+            className={cn([buttonStyles, drawMode === "polygon" ? "bg-emerald-400/35" : ""])}
+            onClick={() => handleChangeDrawMode("polygon")}
+          >
+            <Hexagon className={iconStyles} />
+          </button>
         </div>
       </div>
     </div>
