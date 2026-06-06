3a6f6feb168f671421f75701076e5c224e162568
Dan Sapp <dan.sapp@storable.com>
2026-06-06 15:59:19 -0500

    lint

--- src/scMap/ui/centerPanel/useCenterPanel.tsx
+++ src/scMap/ui/centerPanel/useCenterPanel.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, type Dispatch, type SetStateAction } from "react";
 import { useSelector } from "react-redux";
-import type { mapState } from "@/store/slices/mapSlice";
+// import type { mapState } from "@/store/slices/mapSlice";
 import type { drawState } from "@/store/slices/drawSlice";
 import type { vesselState } from "@/store/slices/vesselSlice";
 import type { AircraftState } from "@/store/slices/aircraftSlice";
@@ -16,7 +16,7 @@ export interface ICenterPanel {
 const useCenterPanel = (): ICenterPanel => {
   const { tab } = useSelector((state: { tab: TabState }) => state.tab);
   const [activeTab, setActiveTab] = useState<TabId>(tab);
-  const { dataLayer } = useSelector((state: { map: mapState }) => state.map);
+  // const { dataLayer } = useSelector((state: { map: mapState }) => state.map);
   const { selectedEntity } = useSelector((state: { draw: drawState }) => state.draw);
   const { selectedVessel } = useSelector((state: { vessels: vesselState }) => state.vessels);
   const { selectedAircraft } = useSelector((state: { aircraft: AircraftState }) => state.aircraft);
